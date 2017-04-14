01bd0c1da45e9e4cb38cfa74cdcecfe85b2659a9
lixz5 <lixz5@lenovo.com>
2017-04-14 15:36:13 +0800

    first add TimeWidget

---
+++ TimeWidget/app/src/main/java/com/lenovo/timewidget/MyAppWidgetProvider.java
@@ -0,0 +1,26 @@
+package com.lenovo.timewidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N=appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            int appWidgetId=appWidgetIds[i];
+            RemoteViews views=new RemoteViews(context.getPackageName(),R.layout.firstappwidget);
+            java.text.DateFormat df=new SimpleDateFormat("hh:mm:ss");
+            views.setTextViewText(R.id.tvMsg,"当前时间:"+df.format(new Date()));
+            appWidgetManager.updateAppWidget(appWidgetId,views);
+            Log.v("TAG","MyAppWidgetProvider");
+        }
+    }
+}
