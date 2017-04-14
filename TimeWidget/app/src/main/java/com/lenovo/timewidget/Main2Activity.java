7c23abb5470820a9f57996dee9d1128a5b8ab5b5
lixz5 <lixz5@lenovo.com>
2017-04-14 16:01:35 +0800

    Main2Activity.java is change

--- TimeWidget/app/src/main/java/com/lenovo/timewidget/Main2Activity.java
+++ TimeWidget/app/src/main/java/com/lenovo/timewidget/Main2Activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Main2Activity extends Activity {
 
+//ÎÒÐÞ¸ÄµÄ
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
